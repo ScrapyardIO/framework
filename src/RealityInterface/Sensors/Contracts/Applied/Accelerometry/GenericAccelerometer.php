c351d789e6af055acc2795b974164ea58e145fca
Angel Gonzalez <usftechacg@gmail.com>
2026-06-05 12:34:13 -0400

    Accelerometry
    + removing the suffix from the License.

--- src/RealityInterface/Sensors/Contracts/Applied/Accelerometry/GenericAccelerometer.php
+++ src/RealityInterface/Sensors/Contracts/Applied/Accelerometry/GenericAccelerometer.php
@@ -4,10 +4,6 @@ namespace RealityInterface\Sensors\Contracts\Applied\Accelerometry;
 
 interface GenericAccelerometer
 {
-    /**
-     * Read the current acceleration on all three axes.
-     *
-     * @return array{x: float, y: float, z: float} Values in g.
-     */
-    public function readXYZ(): array;
+
+    public function getAcceleration(): array;
 }
